8c34a9c4b9abf46e29523672f91caf77e1bc2d40
sree714 <smritysreebanerjee714@gmail.com>
2021-07-21 08:02:45 +0530

    Home activity created & design done

--- app/src/main/java/com/indiastastebook/MainActivity.java
+++ app/src/main/java/com/indiastastebook/MainActivity.java
@@ -3,13 +3,28 @@ package com.indiastastebook;
 //sha1 key
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int SPLASH_SCREEN_TIME_OUT = 5000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this, HomeActivity.class));
+                //overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                finish();
+                //the current activity will get finished.
+            }
+        }, SPLASH_SCREEN_TIME_OUT);
     }
 }
