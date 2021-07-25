66b585dd6bf213792c1aec5fefe0a2c127ecae21
sree714 <smritysreebanerjee714@gmail.com>
2021-07-25 09:10:54 +0530

    Google Sigh in done

--- app/src/main/java/com/indiastastebook/activity/HomeActivity.java
+++ app/src/main/java/com/indiastastebook/activity/HomeActivity.java
@@ -15,8 +15,21 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.indiastastebook.R;
 import com.indiastastebook.fragment.FavouriteFragment;
 import com.indiastastebook.fragment.HomeFragment;
@@ -25,17 +38,39 @@ import com.indiastastebook.utils.App;
 
 public class HomeActivity extends AppCompatActivity {
 
+    //view
     private DrawerLayout drawerLayout;
     private Toolbar toolbar;
     private Button logout;
     private NavigationView navigation;
     private Fragment selectedFragment;
 
+    //global variable
+    private FirebaseAuth mAuth;
+    private GoogleSignInClient mGoogleSignInClient;
+    private static final int RC_SIGN_IN = 1;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         initId();
+
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        mAuth = FirebaseAuth.getInstance();
+
+
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+
+
         openFragment(new HomeFragment());
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -53,7 +88,7 @@ public class HomeActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.menu_home:
-                       openFragment(new HomeFragment());
+                        openFragment(new HomeFragment());
                         break;
 
                     case R.id.menu_select_language:
@@ -81,8 +116,51 @@ public class HomeActivity extends AppCompatActivity {
         navigation = findViewById(R.id.nav_view);
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                // Log.d(TAG, "firebaseAuthWithGoogle:" + account.getId());
+                firebaseAuthWithGoogle(account.getIdToken());
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                // Log.w(TAG, "Google sign in failed", e);
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            //Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            // updateUI(user);
+                            Toast.makeText(HomeActivity.this, "Sigh in successful", Toast.LENGTH_SHORT).show();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            // Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            //  updateUI(null);
+                            Toast.makeText(HomeActivity.this, "Sigh in not successful", Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+
+                });
+    }
+
     private void openFragment(Fragment fragment) {
-        selectedFragment =  fragment;
+        selectedFragment = fragment;
         FragmentManager manager = getSupportFragmentManager();
         manager.beginTransaction().replace(R.id.frame_layout, selectedFragment).commit();
 
