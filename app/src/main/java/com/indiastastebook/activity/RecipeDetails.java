04cbaa6b2fedae942f740d8dcf1a7f61612ef9c3
sree714 <smritysreebanerjee714@gmail.com>
2021-08-02 20:11:15 +0530

    design recipe details

--- app/src/main/java/com/indiastastebook/activity/RecipeDetails.java
+++ app/src/main/java/com/indiastastebook/activity/RecipeDetails.java
@@ -2,15 +2,46 @@ package com.indiastastebook.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.indiastastebook.R;
 
 public class RecipeDetails extends AppCompatActivity {
+    private ImageView backImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_details);
+        init();
+
+        backImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+
     }
+
+    private void init() {
+        backImage = findViewById(R.id.recipe_details_back);
+    }
+
+    @Override
+    public void startActivity(Intent intent) {
+        super.startActivity(intent);
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+    }
+
 }
