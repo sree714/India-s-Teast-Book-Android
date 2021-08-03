8bea71701a63fc2c2250f65abf88a75397a7cc23
soumoq <soumojyotiganguly270@gmail.com>
2021-08-03 08:09:22 +0530

    get data from firebase

--- app/src/main/java/com/indiastastebook/fragment/RegionFragment.java
+++ app/src/main/java/com/indiastastebook/fragment/RegionFragment.java
@@ -1,6 +1,7 @@
 package com.indiastastebook.fragment;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +12,12 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.indiastastebook.R;
 import com.indiastastebook.adaptor.FoodList;
 
@@ -39,6 +46,38 @@ public class RegionFragment extends Fragment {
         list.add("https://firebasestorage.googleapis.com/v0/b/india-s-taste-book.appspot.com/o/fish%20curry.jpg?alt=media&token=c71d6570-33f2-467b-ac20-b695374a170b");
         list.add("https://firebasestorage.googleapis.com/v0/b/india-s-taste-book.appspot.com/o/Shukto.jpg?alt=media&token=4dfe3166-c4e7-44de-9d6e-f771b3175bb4");
         food.updateData(list);
+
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        CollectionReference mainCollection = db.collection("Food Table");
+        mainCollection.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    QuerySnapshot document = task.getResult();
+                    for (int i = 0; i < document.getDocuments().size(); i++) {
+                        Log.e("LOGDATA", document.getDocuments().get(i).getData().get("Region").toString());
+                        String docId = document.getDocuments().get(i).getId();
+                        CollectionReference recipe = mainCollection.document(docId).collection("Recipe");
+                        recipe.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    QuerySnapshot document1 = task.getResult();
+                                    for (int j = 0; j < document1.getDocuments().size(); j++) {
+                                        Log.e("LOGDATA_1", document1.getDocuments().get(j).getData().toString());
+                                    }
+
+                                }
+                            }
+                        });
+                    }
+
+                }
+            }
+        });
+
+
         return view;
     }
 
